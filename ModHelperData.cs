ffc402d6dc707e276464b240311e1db407a64d7b
RedBlooN-YT <monkeyfiesta2@gmail.com>
2026-06-02 10:59:35 -0700

    Add files via upload

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.1";
     public const string Name = "PVZTowers";
 
-    public const string Description = "A Mod That Adds Plants From PVZ to the game. Also has a plant food mechanic that applies a buff that only works on plants and does different things depending on the plant. Planned Updates: Definitely new plants, and maybe pvz boss bloons! There is a bug with teh plant food where sometimes it does nothing the first time it is applied hopefully that will be fixed soon as well.";
+    public const string Description = "A Mod That Adds Plants From PVZ to the game. Also has a plant food mechanic that applies a buff that only works on plants and does different things depending on the plant. Planned Updates: Definitely new plants, and maybe pvz boss bloons! There is a bug with the plant food where sometimes it does nothing the first time it is applied hopefully that will be fixed soon as well. Now has a setting to turn on PVZ mode allowing the plants to be plced on the track to block bloons and take damag while blocking them. This allows it to feel more like the PVZ games.";
 
     public const string RepoOwner = "RedBlooN-YT";
     public const string RepoName = "PVZ-Towers";
